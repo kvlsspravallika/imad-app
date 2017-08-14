9bcd153ec0d69990a34781adc9a587dd7aee0f25
kvlsspravallika <30955757+kvlsspravallika@users.noreply.github.com>
2017-08-14 20:21:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick=function()
     
     request.onreadystatechange=function()
     {
-     if(request.readystate===XMLHttpRequest.DONE)
+     if(request.readyState===XMLHttpRequest.DONE)
      {
      //PERFORM SOME ACCTION
      
@@ -32,16 +32,24 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list
+            }
+        }
+    };
     
-    var names=['name1','name2','namae3'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-        
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-    
+    request.open('GET','http://kvlsspravallika.imad.hasura-app.io/submit-name?'+name,true);
+   request.send(null);
 };
 
 
