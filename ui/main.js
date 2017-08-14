4368fb1cab425ab2f1b4ff81f70fad015f78ee8c
kvlsspravallika <30955757+kvlsspravallika@users.noreply.github.com>
2017-08-14 09:45:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick=function()
      if(request.status==200)
      {
         var counter= request.responseText;
-        var span=getElementById("counter");
+        var span=getElementById("count");
         span.innerHTML=counter.toString();
      }
      }
