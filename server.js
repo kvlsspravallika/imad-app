ba9eb1e1351d9ea85d82e455be05184834cbecf6
kvlsspravallika <30955757+kvlsspravallika@users.noreply.github.com>
2017-08-14 08:27:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/maIN.JS', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
