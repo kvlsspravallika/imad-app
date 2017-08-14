edad1bfe08b5ed79455187af703e99d3818cf3c8
kvlsspravallika <30955757+kvlsspravallika@users.noreply.github.com>
2017-08-14 08:45:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var marginLeft;
 function moveRight()
 {
    marginLeft=marginLeft+10;
-   img.style.marginLeft=marginLeft+'px';
+   img.style.marginLeft=marginLeft +'px';
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,100);
