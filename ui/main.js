b45f4eb95e48bf85719282cf9b57655270b08c41
kvlsspravallika <30955757+kvlsspravallika@users.noreply.github.com>
2017-08-14 19:56:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,8 +35,8 @@ submit.onclick=function(){
     
     var names=['name1','name2','namae3'];
     var list='';
-    for(var i=0;i<names.length();i++){
-        list+='<li>'+names[i]+'</li';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
         
     }
     var ul=document.getElementById('namelist');
