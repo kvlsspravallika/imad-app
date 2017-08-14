89471db53cbe5f0341062c2515e3b8e54ca16001
kvlsspravallika <30955757+kvlsspravallika@users.noreply.github.com>
2017-08-14 20:29:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,7 +49,7 @@ submit.onclick=function(){
     
     var nameInput=document.getElementById('name');
 var name=nameInput.value;
-    request.open('GET','http://kvlsspravallika.imad.hasura-app.io/submit-name?'+name,true);
+    request.open('GET','http://kvlsspravallika.imad.hasura-app.io/submit-name?name='+name,true);
    request.send(null);
 };
 
